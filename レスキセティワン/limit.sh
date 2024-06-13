8096bf791ba6b8072fe30550d510315ea042e824
Ryuchiii <146633066+Ryuchiii@users.noreply.github.com>
2024-06-13 23:53:07 +0700

    Update limit.sh

--- レスキセティワン/limit.sh
+++ レスキセティワン/limit.sh
@@ -1,7 +1,7 @@
 # =========================================
 # Menu For Script
 # Edition : Stable Edition V2.0
-# Auther  : Tekirovpn & Lynzvpn & Prof.Ampoh
+# Auther  : TekiroVpn X LynzVpn
 # (C) Copyright 2023-2024
 # =========================================
 REPO="https://aio.tekirovpn.my.id/"
